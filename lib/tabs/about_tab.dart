b8e5b9afc4cba1eb7b6b340212833fc00a6e3f03
Thesohan <sohan.cse16@nituk.ac.in>
2020-03-27 02:07:13 +0530

    final commit

--- lib/tabs/about_tab.dart
+++ lib/tabs/about_tab.dart
@@ -8,40 +8,36 @@ import 'package:my_protfolio/widgets/responsive_widget.dart';
 class AboutTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 16.0),
-          child: ListView(
-            primary: true,
-            shrinkWrap: true,
-            children: <Widget>[
-              ResponsiveWidget(
-                largeScreen: GridView.count(
-                  primary: false,
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  childAspectRatio: 14.0 / 6.0,
-                  children: List.generate(
-                    companies.length,
-                    (index) => CompanyWidget(
-                      companyModel: companies[index],
-                    ),
-                  ),
-                ),
-                smallScreen: ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  itemCount: projects.length,
-                  itemBuilder: (context, index) => CompanyWidget(
-                    companyModel: companies[index],
-                  ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: ListView(
+        primary: true,
+        shrinkWrap: true,
+        children: <Widget>[
+          ResponsiveWidget(
+            largeScreen: GridView.count(
+              primary: false,
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              childAspectRatio: 14.0 / 6.0,
+              children: List.generate(
+                companies.length,
+                (index) => CompanyWidget(
+                  companyModel: companies[index],
                 ),
               ),
-              SocialProfile(),
-            ],
+            ),
+            smallScreen: ListView.builder(
+              shrinkWrap: true,
+              primary: false,
+              itemCount: projects.length,
+              itemBuilder: (context, index) => CompanyWidget(
+                companyModel: companies[index],
+              ),
+            ),
           ),
-        ),
+          SocialProfile(),
+        ],
       ),
     );
   }
